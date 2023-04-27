77fb5250beb2780f3dc486d92f3255e37e44b27e
wnetohr <washingtonbdarocha@gmail.com>
2023-04-27 14:58:51 -0300

    menu navi fades after the user click in some option

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const menu = document.querySelector('.menu');
 const menuNavi = document.querySelector('.menu-navi');
+const btnHome = document.querySelector('.btnHome');
+const btnProject = document.querySelector('.btnProject');
+const btnContact = document.querySelector('.btnContact');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('ativo');
     menuNavi.classList.toggle('ativo');
-})
+});
+btnHome.addEventListener('click', () => {
+    menu.classList.remove('ativo');
+    menuNavi.classList.remove('ativo');
+});
+btnProject.addEventListener('click', () => {
+    menu.classList.remove('ativo');
+    menuNavi.classList.remove('ativo');
+});
+btnContact.addEventListener('click', () => {
+    menu.classList.remove('ativo');
+    menuNavi.classList.remove('ativo');
+});
